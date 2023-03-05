56d36afd8872d136870eb5511e4ee6921191e13a
chriskim2003 <chriskim2003122@gmail.com>
2023-03-05 22:10:18 +0900

    git ignore pom.xml, .idea

--- src/main/java/SiruMod/relics/SiruBasicRelic.java
+++ src/main/java/SiruMod/relics/SiruBasicRelic.java
@@ -4,6 +4,7 @@ import SiruMod.DefaultMod;
 import SiruMod.util.TextureLoader;
 import basemod.abstracts.CustomRelic;
 import com.badlogic.gdx.graphics.Texture;
+import com.evacipated.cardcrawl.mod.stslib.util.UpgradeData;
 import com.megacrit.cardcrawl.actions.AbstractGameAction;
 import com.megacrit.cardcrawl.actions.GameActionManager;
 import com.megacrit.cardcrawl.actions.unique.MadnessAction;
@@ -11,11 +12,14 @@ import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.cards.CardGroup;
 import com.megacrit.cardcrawl.cards.CardQueueItem;
 import com.megacrit.cardcrawl.cards.colorless.Madness;
+import com.megacrit.cardcrawl.cards.status.Dazed;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.helpers.CardHelper;
 import com.megacrit.cardcrawl.helpers.CardLibrary;
 import com.megacrit.cardcrawl.relics.AbstractRelic;
+
+import javax.smartcardio.Card;
 
 public class SiruBasicRelic extends CustomRelic {
 
@@ -23,13 +27,20 @@ public class SiruBasicRelic extends CustomRelic {
 
     private static final Texture IMG = TextureLoader.getTexture("SiruModResources/images/relics/SiruBasic_Relic.png");
     public SiruBasicRelic() {
-        super(ID, IMG, RelicTier.SPECIAL, LandingSound.HEAVY);
+        super(ID, IMG, RelicTier.SPECIAL, LandingSound.SOLID);
     }
 
     @Override
     public void atTurnStartPostDraw() {
         flash();
-        AbstractDungeon.player.useCard(CardLibrary.getCard(Madness.ID), null, 0);
+        AbstractCard madness = CardLibrary.getCard(Madness.ID);
+        madness.upgrade();
+        AbstractDungeon.player.useCard(madness, null, 0);
+    }
+
+    @Override
+    public void onPlayerEndTurn() {
+        AbstractDungeon.player.discardPile.addToTop(CardLibrary.getCard(Dazed.ID));
     }
 
     @Override
